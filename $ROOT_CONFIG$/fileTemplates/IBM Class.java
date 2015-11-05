ab097cfad82123cc30406db1d994f0a940088b0f
yli <yli@liyixa-w530.spss.com>
2015-11-05 14:27:08 +0800

    IC-15.0 <yli@liyixa-w530 Create markdown.xml, ide.general.xml, Default.xml, git.xml, project.default.xml, diff.xml, other.xml, IBM Enum.java, terminal.xml, IBM Singleton.java, applicationLibraries.xml, file.template.settings.xml, Solarized Dark.icls, find.xml, ui.lnf.xml, filetypes.xml, keymap.xml, androidEditors.xml, code.style.schemes.xml, plugin_ui.xml, colors.scheme.xml, notifications.xml, Solarized Dark (Darcula).icls, Solarized Light.icls, Ricky.xml, Solarized Light (Darcula).icls, Default copy.xml, scala.xml, laf.xml, jdk.table.xml, debugger.renderers.xml, IBM-File-Header.java, path.macros.xml, web-browsers.xml, proxy.settings.xml, IBM Interface.java, mavenServices.xml, IBM Class.java, editor.xml, debugger.xml

---
+++ $ROOT_CONFIG$/fileTemplates/IBM Class.java
@@ -0,0 +1,6 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+
+#parse("IBM-File-Header.java")
+
+public class ${NAME} {
+}
---
+++ $ROOT_CONFIG$/fileTemplates/includes/IBM-File-Header.java
@@ -0,0 +1,7 @@
+/*
+@Copyright("\r\n\r\nLicensed Materials - Property of IBM\r\n\r\n" +
+        "IBM SPSS Products: Modeler Desktop\r\n\r\n" +
+        "\u00a9 Copyright IBM Corp. 1994, ${YEAR}\r\n\r\n" +
+        "US Government Users Restricted Rights - Use, duplication or disclosure restricted by GSA ADP\r\n" +
+        "Schedule Contract with IBM Corp.\r\n\r\n")
+*/
